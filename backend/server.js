b4c7ffd6df3fd5d8ee07900eab4afe45a3d4c059
Yohanan Kling <yohanankli@gmail.com>
2025-07-14 20:13:11 +0300

    receive data from all users, *not* accurate fields

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const { sequelize, initModels } = require('./models');
 const managerRoutes = require('./routes/manager_routes');
 const authRoutes = require('./routes/auth_routes');
-// const communityRoutes = require('./routes/community_routes');
+const communityRoutes = require('./routes/community_routes');
 
 const app = express();
 app.use(cors());
@@ -12,15 +12,15 @@ app.use(express.json());
 
 app.use('/managers', managerRoutes);
 app.use('/auth', authRoutes);
-// app.use('/communities', communityRoutes);
+app.use('/communities', communityRoutes);
 
 
 sequelize.authenticate()
-  .then(() => {
-    console.log('✅ DB connected');
-    return initModels();
-  })
-  .then(() => {
-    app.listen(5000, () => console.log('🚀 Server running on http://localhost:5000'));
-  })
-  .catch((err) => console.error('❌ DB error:', err));
+    .then(() => {
+        console.log('✅ DB connected');
+        return initModels();
+    })
+    .then(() => {
+        app.listen(5000, () => console.log('🚀 Server running on http://localhost:5000'));
+    })
+    .catch((err) => console.error('❌ DB error:', err));
